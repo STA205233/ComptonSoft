e1c44763d74ea3beb16531d69c6ddfa864a6c992
ShotaArai <shota.arai@phys.s.u-tokyo.ac.jp>
2026-09-06 05:12:23 +0900

    tweaks

--- source/applications/missions/nanograms/include/LightData.hh
+++ source/applications/missions/nanograms/include/LightData.hh
@@ -19,9 +19,25 @@
 
 #ifndef COMPTONSOFT_LightWaveform_hh
 #define COMPTONSOFT_LightWaveform_hh 1
+#include <map>
+#include <vector>
 namespace comptonsoft {
-class LightWaveform
+class LightData
 {
+  using value_t = double;
+  LightData();
+  virtual ~LightData();
+
+public:
+  std::vector<value_t>& waveform(int channel)
+  {
+    static std::vector<value_t> empty(0);
+    const auto iter = waveforms_.find(channel);
+    return iter != waveforms_.end() ? iter->second : empty;
+  }
+
+private:
+  std::map<int, std::vector<value_t>> waveforms_; // ch, vector
 };
 } // namespace comptonsoft
 #endif
